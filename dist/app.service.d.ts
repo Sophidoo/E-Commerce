641cefc389b0a75c6cb83a3fd771efa5de9faf35
Sophia Okosodo <sophieokosodo@gmail.com>
2023-09-03 23:42:09 +0100

    databse seeding

--- dist/app.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnApplicationBootstrap } from '@nestjs/common';
-import { PrismaService } from './database/prisma.service';
-export declare class AppService implements OnApplicationBootstrap {
-    private readonly prismaService;
-    constructor(prismaService: PrismaService);
-    onApplicationBootstrap(): Promise<void>;
-    hashPassword(password: string): Promise<string>;
-}
